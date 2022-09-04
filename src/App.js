fbd57e11fa8e80ed71da099667babd87ed06d10e
JunSeong <gentlmanjun@naver.com>
2022-09-05 02:42:55 +0900

    문장 길이에 맞춰 출력& 다른 폰트 테스트

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import logo from "./logo.svg";
 import "./App.css";
 import { load } from "opentype.js";
-import Roboto from "./font/BlackHanSans.woff";
+// import Roboto from "./font/BlackHanSans.woff";
+import Roboto from "./font/NanumGothic.woff";
 import { useEffect } from "react";
+
+const text = "테스트중입니다.";
+const textLen = text.length;
 
 function App() {
   useEffect(() => {
@@ -13,33 +17,33 @@ function App() {
       if (err) {
         alert("Font could not be loaded: " + err);
       } else {
-        const glyphs = font.stringToGlyphs("헬로 월드!");
+        const glyphs = font.stringToGlyphs(text);
         console.log(glyphs);
-        // for (let i = 0; i < glyphs.length; i++) {
-        for (let i = 0; i < 1; i++) {
+        for (let i = 0; i < glyphs.length; i++) {
+          // for (let i = 0; i < 1; i++) {
           console.log(glyphs[i].path.commands);
           for (let j = 0; j < glyphs[i].path.commands.length; j++) {
             const command = glyphs[i].path.commands[j];
-            draw(ctx, command);
+            draw(ctx, command, i);
           }
         }
       }
     });
   }, []);
 
-  const draw = (ctx, command) => {
+  const draw = (ctx, command, order) => {
     console.log("draw ");
     // console.log(command);
     if (command.type === "M") {
       ctx.beginPath();
-      ctx.moveTo(command.x, 900 - command.y);
+      ctx.moveTo(command.x + 900 * order, 900 - command.y);
     } else if (command.type === "L") {
-      ctx.lineTo(command.x, 900 - command.y);
+      ctx.lineTo(command.x + 900 * order, 900 - command.y);
     } else if (command.type === "Q") {
       ctx.quadraticCurveTo(
-        command.x1,
+        command.x1 + 900 * order,
         900 - command.y1,
-        command.x,
+        command.x + 900 * order,
         900 - command.y
       );
     } else if (command.type === "Z") {
@@ -54,7 +58,7 @@ function App() {
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header> */}
-      <canvas id="canvas" width="900" height="900"></canvas>
+      <canvas id="canvas" width={textLen * 900} height="1100"></canvas>
     </div>
   );
 }
