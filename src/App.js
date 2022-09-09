a94990694bf2fc602a6b879b15f9229ce29c57b2
JunSeong <gentlmanjun@naver.com>
2022-09-09 10:40:06 +0900

    글자 입력에 따라서 리렌더링

--- src/App.js
+++ src/App.js
@@ -3,15 +3,27 @@ import "./App.css";
 import { load } from "opentype.js";
 // import Roboto from "./font/BlackHanSans.woff";
 import Roboto from "./font/NanumGothic.woff";
-import { useEffect } from "react";
-
-const text = "테스트중입니다.";
-const textLen = text.length;
+import { useEffect, useState } from "react";
 
 function App() {
+  const [text, setText] = useState("");
+  const [textLen, setTextLen] = useState(0);
+
+  const onChange = (e) => {
+    setText(e.target.value);
+    setTextLen(e.target.value.length * 900);
+  };
+
+  const onReset = () => {
+    setText("");
+    setTextLen(0);
+  };
+
   useEffect(() => {
     const canvas = document.getElementById("canvas");
     const ctx = canvas.getContext("2d");
+
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     load(Roboto, (err, font) => {
       if (err) {
@@ -28,12 +40,32 @@ function App() {
           }
         }
       }
+
+      // const svg = document.getElementById("canvas");
+      // const warp = new Warp(svg);
+
+      // warp.interpolate(4);
+      // warp.transform(([x, y]) => [x, y, y]);
+
+      // let offset = 0;
+      // function animate() {
+      //   warp.transform(([x, y, oy]) => [
+      //     x,
+      //     oy + 4 * Math.sin(x / 16 + offset),
+      //     oy,
+      //   ]);
+      //   offset += 0.1;
+      //   requestAnimationFrame(animate);
+      // }
+
+      // animate();
     });
-  }, []);
+  }, [text]);
 
   const draw = (ctx, command, order) => {
     console.log("draw ");
     // console.log(command);
+    ctx.lineWidth = 20;
     if (command.type === "M") {
       ctx.beginPath();
       ctx.moveTo(command.x + 900 * order, 900 - command.y);
@@ -55,10 +87,18 @@ function App() {
 
   return (
     <div className="App">
+      <input onChange={onChange} value={text} />
+      <button onClick={onReset}>초기화</button>
+      <div>
+        <b> 값 : {text}</b>
+      </div>
+      <div>
+        <b> 값 : {textLen}</b>
+      </div>
       {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
       </header> */}
-      <canvas id="canvas" width={textLen * 900} height="1100"></canvas>
+      <canvas id="canvas" width={textLen} height="1100"></canvas>
     </div>
   );
 }
